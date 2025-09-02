7cde6bcbe17d05e902e28067fad9932a4c2b6180
Renanbomfim35 <renan.bomfim@yahoo.com.br>
2025-09-02 00:29:30 +0000

    criar classe televisao

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
+
+Televisao tv = new Televisao();
+
+tv.Tamanho = 22.5f;
+
+Console.WriteLine($" A tv tem o tamanho{ tv.Tamanho}");
+
+tv.Volume = -35;
+Console.WriteLine($"volume{tv.Volume}");
+
+tv.Volume = 120;
+
+Console.WriteLine($"volume{tv.Volume}");
